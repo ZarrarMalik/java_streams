d01ccc52f553dfac4e64fc96ce46d0f279eac20b
zarrar <zarrarmalik81@gmail.com>
2020-05-05 16:15:39 +0100

    Use intStream range

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -8,22 +8,42 @@ import org.junit.Test;
 
 public class Lecture2 {
 
-  @Test
-  public void range() throws Exception {
+	@Test
+	public void range() throws Exception {
+		// Simple for loop printing 1 to 10
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
+		// Prints 0 to 9
+		IntStream.range(0, 10).forEach(System.out::println);
+		// Prints 0 t0 10
+		IntStream.rangeClosed(0, 10).forEach(System.out::println);
+	}
 
-  }
+	@Test
+	public void rangeIteratingLists() throws Exception {
+		List<Person> people = MockData.getPeople();
+		/*
+		 * Here we will use range rather than rangeclosed otherwise it throws
+		 * arrayoutofbound Exception we use IntStream to get the indexes of elements of
+		 * the list index just return the solutino of {}
+		 */
 
-  @Test
-  public void rangeIteratingLists() throws Exception {
-    List<Person> people = MockData.getPeople();
+		IntStream.range(0, people.size()).forEach(index -> {
+			Person person = people.get(index);
+			System.out.println(person);
+		});
 
-  }
+	}
 
-  @Test
-  public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
-  }
+	@Test
+	public void intStreamIterate() throws Exception {
+		/*
+		 * returns the numbers from 0 to the limit operand + 1 adds the initial value
+		 * mod gets the even number
+		 */
+
+		IntStream.iterate(0, operand -> operand + 1).filter(number -> number % 2 == 0).limit(20)
+				.forEach(System.out::println);
+	}
 }
