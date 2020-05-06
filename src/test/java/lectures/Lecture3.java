8b6813cb97b951b8499bf5f220e35291588770c6
zarrar <zarrarmalik81@gmail.com>
2020-05-06 10:41:45 +0100

    Use min and max stream methods

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -12,12 +12,30 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    
+	int minNumber = numbers.stream().min((number1, number2)-> number1 > number2 ? 1 : -1).get();
+	// we use .get() because min returns an optional 
+	// also min() use comparator which requires two numbers
+    assertThat(minNumber == 1);
+  }
+  
+  @Test
+  public void minWithComparator() throws Exception {
+    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    /*
+     * we use natural order Comparator to get min number
+     * There are other Comparators as well like reverse order
+     * Type Comparator then . to get the list
+     */
+	int minNumber = numbers.stream().min(Comparator.naturalOrder()).get();
+    assertThat(minNumber == 1);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    int maxNumber = numbers.stream().max(Comparator.naturalOrder()).get();
+    assertThat(maxNumber == 99);
 
   }
 }
