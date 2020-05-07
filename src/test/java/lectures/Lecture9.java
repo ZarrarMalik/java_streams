a5e4d1961f7a57edd840d5e437746ce47f6de7b5
zarrar <zarrarmalik81@gmail.com>
2020-05-07 13:16:19 +0100

    Use stream with reduce

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,18 +1,26 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Arrays;
+
 import org.junit.Test;
 
 public class Lecture9 {
 
-  @Test
-  public void reduce() throws Exception {
-    Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
+    @Test
+    public void reduce() throws Exception {
+        Integer[] integers = { 1, 2, 3, 4, 99, 100, 121, 1302, 199 };
+        // we use reduce with integer identity which is start of the stream
+        // 0 in this case and add the previous number to the next
+        int sumOfGivenArray = Arrays.stream(integers).reduce(0, (a, b) -> a + b);
+        System.out.println(sumOfGivenArray);
+        assertThat(sumOfGivenArray).isNotNull().isGreaterThanOrEqualTo(1831);
 
-  }
-
-
+        // Easier way to do this is via a built in function
+        int sumWithIntegerMethod = Arrays.stream(integers).reduce(0, (Integer::sum));
+        System.out.println(sumWithIntegerMethod);
+        assertThat(sumWithIntegerMethod).isNotNull().isGreaterThanOrEqualTo(1831);
+    }
 }
-
