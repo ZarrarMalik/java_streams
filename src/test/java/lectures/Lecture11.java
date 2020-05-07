97e26b182b62473c24427d83c3850a1c9f3adac3
zarrar <zarrarmalik81@gmail.com>
2020-05-07 14:27:01 +0100

    Use streams for string joining

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,41 @@
 package lectures;
 
-import com.google.common.collect.ImmutableList;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
-import mockdata.MockData;
+
 import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
 
 public class Lecture11 {
 
-  @Test
-  public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    @Test
+    public void joiningStrings() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        String PREFIX = "[";
+        String SUFFIX = "]";
+        StringJoiner joiner = new StringJoiner(",", PREFIX, SUFFIX);
+        for (String name : names) {
 
-  }
+            joiner.add(name);
+        }
+        System.out.println(joiner.toString());
+        assertThat(joiner.toString()).isNotEmpty();
+    }
 
-  @Test
-  public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    @Test
+    public void joiningStringsWithStream() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        String nameWithComma = names.stream().collect(Collectors.joining(",", "{", "}"));
+        System.out.println(nameWithComma);
 
-  }
+        String nameWithMap = names.stream().map(String::toUpperCase)
+                .collect(Collectors.joining(".", "#", "£"));
+        System.out.println(nameWithMap);
+        
+        assertThat(nameWithMap).contains("MARCOS");
+    }
 }
