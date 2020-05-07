ab0e826b2e197d6d25016627f35fc8113a998572
zarrar <zarrarmalik81@gmail.com>
2020-05-07 15:10:48 +0100

    Use Collectors.tolist in different ways

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -17,4 +17,28 @@ public class Lecture12 {
 
     emails.forEach(System.out::println);
   }
+
+    @Test
+    public void TestUnderstandingCollectSecondWay() throws Exception {
+        // Collectors.tolist described further
+        // Use three constructors with supplier, accumulatro and combiner
+        // Does the same job as the above test case
+        List<String> emails = MockData.getPeople().stream().map(Person::getEmail).collect(
+                () -> new ArrayList<>(), (list, item) -> list.add(item),
+                (list1, list2) -> list1.addAll(list2));
+
+        emails.forEach(System.out::println);
+    }
+    
+    @Test
+    public void TestUnderstandingCollectThirdWay() throws Exception {
+        // Collectors.tolist described further
+        // Use three constructors with supplier, accumulatro and combiner
+        // Does the same job as the above test case
+        List<String> emails = MockData.getPeople().stream().map(Person::getEmail).collect(
+                ArrayList::new, ArrayList::add,
+                ArrayList::addAll);
+
+        emails.forEach(System.out::println);
+    }
 }
