2cc1b614faaaeb69bd693ebb02e60610808ba487
zarrar <zarrarmalik81@gmail.com>
2020-05-07 13:58:24 +0100

    Use flat map

--- src/test/java/lectures/Lecture10.java
+++ src/test/java/lectures/Lecture10.java
@@ -2,6 +2,9 @@ package lectures;
 
 
 import com.google.common.collect.Lists;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,17 +24,25 @@ public class Lecture10 {
     System.out.println(arrayListOfNames);
   }
 
-  @Test
-  public void withoutFlatMap() throws Exception {
-//    [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+    @Test
+    public void withoutFlatMap() throws Exception {
+        // Print the names from the arrayListOfNames in a single list
+        // [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> nameOfPeople = new ArrayList<>();
+        for (List<String> names : arrayListOfNames) {
+            for (String name : names) {
+                nameOfPeople.add(name);
+            }
+        }
+        System.out.println(nameOfPeople);
+    }
 
-  }
-
-  @Test
-  public void withFlatMap() throws Exception {
-//   [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
-
-  }
-
+    @Test
+    public void withFlatMap() throws Exception {
+        // [Mariam, Alex, Ismail, John, Alesha, Andre, Susy, Ali]
+        List<String> names = arrayListOfNames.stream().flatMap(List::stream)
+                .collect(Collectors.toList());
+        System.out.println(names);
+        assertThat(names).isNotEmpty();
+    }
 }
-
